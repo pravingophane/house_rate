2564dc0afa0521d2d554ea4970766fca23ab0cc9
unknown <pravingophane@gmail.com>
2022-10-28 16:36:39 +0530

    Project configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = "Enter_path"
+
+PORT_NUMBER = 5010
+
+MODEL_FILE_PATH = os.path.join("Pune_House_data","Pune_model.pkl")
+
+JSON_FILE_PATH = os.path.join("Pune_House_data","project_data.json")
